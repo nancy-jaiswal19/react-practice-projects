0b1cd1043a68b8789bdd9e9f61c7c55aa3b501f4
nancy-jaiswal19 <nancyjais1907@gmail.com>
2025-08-22 15:26:06 +0530

    Added React projects

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0);
+  // let counter = 15
+
+  const addValue = () =>{
+    if(counter < 20){
+      console.log("added");
+      // counter = counter + 1
+      setCounter(counter+1)
+    }
+  }
+
+  const removeValue = () =>{
+    if(counter > 0){
+      console.log("removed");
+      setCounter(counter-1)
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter values: {counter}</h2>
+
+      <button onClick={addValue} >Add value</button>
+      
+      <button onClick = {removeValue} >Remove value</button>
+    </>
+  )
+}
+
+export default App
